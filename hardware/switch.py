18d8033e2f5ebfb690d84d8684f14d6122f9334e
jross <jake.ross@nmt.edu>
2023-06-15 13:26:13 -0600

    added ramp sequence

--- hardware/switch.py
+++ hardware/switch.py
@@ -185,14 +185,18 @@ class SwitchController(Device):
         #     if self.canvas:
         #         self.canvas.set_switch_state(s.name, state)
         def script():
-            scriptname = "default"
+            scriptname = 'default'
+            dry = True
             if isinstance(slow, str):
                 scriptname = slow
+                dry = False
 
             st = time.time()
-
-            for idx, row in enumerate(self._load_ramp_script(scriptname)):
-                self._execute_script_row(idx, row, s, st)
+            with open(Path(paths.curves_dir, f'{scriptname}_output.csv'), 'w') as wfile:
+                writer = csv.writer(wfile, delimiter=',')
+                writer.writerow(['step', 'time', 'stepidx', 'output', 'voltage'])
+                for idx, row in enumerate(self._load_ramp_script(scriptname)):
+                    self._execute_script_row(writer, idx, row, s, st, dry=dry)
 
         if block:
             script()
@@ -208,23 +212,23 @@ class SwitchController(Device):
             for row in rows[1:]:
                 yield row
 
-    def _execute_script_row(self, idx, row, s, st):
-        self.debug(f"execute script row. line={idx+1}, {row}")
-        voltage, nsteps, dwelltime, curve = row[:4]
+    def _execute_script_row(self, writer, idx, row, s, st, dry):
+        self.debug(f'execute script row. line={idx+1}, {row}')
+        voltage, n_steps, dwell_time, curve = row[:4]
         voltage = float(voltage)
-        nsteps = int(nsteps)
-        dwelltime = float(dwelltime)
+        n_steps = int(n_steps)
+        dwell_time = float(dwell_time)
         curve = int(curve)
 
         self.debug(f"set output {voltage}")
 
-        def make_curve(curvename, nsteps=100, invert=False):
-            curvepath = Path(paths.curves_dir, f"curve_rates.csv")
-            with open(curvepath, "r") as rfile:
-                reader = csv.reader(rfile, delimiter=",")
+        def make_curve(curve_name, nsteps=100, invert=False):
+            curve_path = Path(paths.curves_dir, f'curve_rates.csv')
+            with open(curve_path, 'r') as rfile:
+                reader = csv.reader(rfile, delimiter=',')
                 rows = [row for row in reader]
-                control_points = rows[curvename - 1]
-                self.debug(f"using control points {control_points}")
+                control_points = rows[curve_name - 1]
+                self.debug(f'using control points {control_points}')
                 n = len(control_points) + 1
                 control_points = [
                     ((i + 1) / n, float(cp) / 100)
@@ -235,18 +239,24 @@ class SwitchController(Device):
                 xs, ys = bezier_curve(control_points, nsteps + 1)
                 if invert:
                     ys = [1 - yi for yi in ys]
-                return ys
+                return ys[:-1]
 
-        for out in make_curve(curve, nsteps):
+        for stepidx, out in enumerate(make_curve(curve, n_steps)):
             vi = voltage * out
             self.debug(f"set output {out}, voltage={vi}")
-            kw = {"relative_time_seconds": time.time() - st, "max_voltage": 7}
-            self._set_voltage(s, vi, **kw)
-            time.sleep(1)
+            ct = time.time() - st
+            kw = {"relative_time_seconds": ct,
+                  "max_voltage": 7}
 
-        if dwelltime:
-            self.debug(f"dwelling {dwelltime}s")
-            time.sleep(dwelltime)
+            self._set_voltage(s, vi, **kw)
+            if not dry:
+                time.sleep(1)
+
+            writer.writerow([idx, ct, stepidx, out, vi])
+
+        if dwell_time and not dry:
+            self.debug(f'dwelling {dwell_time}s')
+            time.sleep(dwell_time)
 
     def _set_voltage(self, s, voltage, **kw):
         self.driver.set_voltage(s.channel, voltage)
